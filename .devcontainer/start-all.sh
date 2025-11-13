2eaf0171e37f37bda544c90c3b672e59a6e607f2
DemonBigj781 <20097331+DemonBigj781@users.noreply.github.com>
2025-11-13 16:55:47 -0500

    Modify JupyterLab command and log handling
    Updated JupyterLab startup command and ensured log files are created before tailing.

--- .devcontainer/start-all.sh
+++ .devcontainer/start-all.sh
@@ -10,12 +10,22 @@ cd /app
 nohup ./horde-scribe-bridge.sh > /var/log/horde-scribe.log 2>&1 &
 
 # Start JupyterLab on port 8888, no token
-nohup jupyter lab --ip 0.0.0.0 --port 8888 --no-browser --NotebookApp.token='' \
+# Use python3 -m jupyterlab so we don't care what the CLI binary is called
+cd /workspaces || cd /app
+nohup python3 -m jupyterlab \
+  --ip=0.0.0.0 \
+  --port=8888 \
+  --no-browser \
+  --ServerApp.token='' \
+  --ServerApp.password='' \
+  --ServerApp.allow_origin='*' \
   > /var/log/jupyter.log 2>&1 &
 
 # Start simple FTP server on port 2121
 nohup python3 -m pyftpdlib -p 2121 > /var/log/ftp.log 2>&1 &
 
-# Keep container alive by tailing logs
+# Make sure logs exist so tail doesn't crash
 touch /var/log/horde-scribe.log /var/log/jupyter.log /var/log/ftp.log
+
+# Keep container alive by tailing logs
 tail -F /var/log/horde-scribe.log /var/log/jupyter.log /var/log/ftp.log
